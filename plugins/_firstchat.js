d8daa80c8371b80d9d31103e301aa169233e73a9
mursidxd <110702247+mursidxd@users.noreply.github.com>
2022-08-29 19:48:22 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -3,15 +3,16 @@ let handler = m => m
 
 handler.all = async function (m) {
 
-    if (m.chat.endsWith('broadcast')) return
+    if (m.chat.endsWith('broadcast') || m.key.remoteJid.endsWith('broadcast')) return
     if (m.fromMe) return
     if (m.isGroup) return
     if (db.data.settings.groupOnly) return
     let user = global.db.data.users[m.sender]
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
-    await this.sendButton(m.chat, `
-Hai, ${ucapan()}
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
+    await this.sendButtonLoc(m.chat, 'https://telegra.ph/file/9d4bde7fc703116b92f64.jpg', `
+Hai kak, ${ucapan()}
+
+${user.banned ? 'kamu dibanned' : 'мυʀѕι∂ вσт-χмℓ adalah bot whatsapp yang di bangun dengan Nodejs, dengan menggunakan server yang kencang, tapi tidak terlalu kencang-kencang banget.'}
 `.trim(), wm, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '.owner' : '.menu', m)
     user.pc = new Date * 1
 }
